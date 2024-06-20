903cd6dc914e4b49a6a6423b9543da417c53e66b
Sithembiso Sangweni <sithembiso72@gmail.com>
2024-06-20 14:28:19 +0200

    latest

--- js/app.js
+++ js/app.js
@@ -11,49 +11,43 @@ let timeDom = document.querySelector('.carousel .time');
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
 let timeAutoNext = 7000;
-console.log(timeAutoNext)
-console.log(timeRunning)
 
 nextDom.onclick = function(){
-    console.log("nextDown clicked")
     showSlider('next');    
 }
 
 prevDom.onclick = function(){
-    console.log("prevDom clicked")
     showSlider('prev');    
 }
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
-    console.log("runNextAuto")
     next.click();
 }, timeAutoNext)
 function showSlider(type){
-    console.log("showSlider")
     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
     
     if(type === 'next'){
-        console.log("next ")
+
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
     }else{
-        console.log("prev ")
+
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
     }
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
-        console.log("timeRunning ")
+
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
     }, timeRunning);
 
     clearTimeout(runNextAuto);
     runNextAuto = setTimeout(() => {
-        console.log("timeAutoNext")
+
         next.click();
     }, timeAutoNext)
 }
